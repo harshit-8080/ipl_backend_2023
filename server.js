0132039d9c1fb37cdef1f8ec5e0ba9c27c35f5f1
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2022-12-24 20:09:31 +0530

    create team api

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 const express = require("express");
+const { TeamRouter } = require("./src/routes/teams.route");
 
+const setupAndStartServer = async function () {
+  const app = express();
 
-const setupAndStartServer = async function(){
+  // app.get("/",(req,res)=>{
 
-    const app = express();
+  //     console.log("Request = ",req);
+  //     console.log("Response = ",res);
 
-    // app.get("/",(req,res)=>{
-      
-    //     console.log("Request = ",req);
-    //     console.log("Response = ",res);
+  //     return res.status(200).json({
+  //         "MSG":"Response from api version 2.0"
+  //     })
+  // })
 
-    //     return res.status(200).json({
-    //         "MSG":"Response from api version 2.0"
-    //     })
-    // })
+  // app.get("/home",(req,res)=>{
 
-    // app.get("/home",(req,res)=>{
-    
-    //     return res.status(200).json({
-    //         "MSG":"home api called"
-    //     })
-    // })
-    // app.get("/contact/harshit",(req,res)=>{
-    
-    //     return res.status(200).json({
-    //         "MSG":"contact api called"
-    //     })
-    // })
+  //     return res.status(200).json({
+  //         "MSG":"home api called"
+  //     })
+  // })
+  // app.get("/contact/harshit",(req,res)=>{
 
+  //     return res.status(200).json({
+  //         "MSG":"contact api called"
+  //     })
+  // })
 
-    app.listen(3000,()=>{
-        console.log("Server started at 3000");
-    })
+  app.use(express.json());
+  app.use(TeamRouter);
 
-}
-
+  app.listen(3000, () => {
+    console.log("Server started at 3000");
+  });
+};
 
 setupAndStartServer();
