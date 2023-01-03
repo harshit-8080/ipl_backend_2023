29980dd1e9bec62a0b745a80dca1461cd179c429
AayushParashar28 <ayushparashar6203@gmail.com>
2023-01-03 21:04:36 +0530

    signin Done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,61 +1,90 @@
 const { User } = require("../models/index");
+const bcrypt = require('bcrypt');
 
-exports.createUser = async (req,res) => {
+exports.createUser = async (req, res) => {
 
-    try {
-        const user = {
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password
-        };
-    
-        const result = await User.create(user);
-    
-        return res.status(201).json({
-          sucess:true,  
-          response: result,
-        });
+  try {
+    const user = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    };
 
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-          msg: "Internal server error",
-        });
-      }
+    const result = await User.create(user);
+
+    return res.status(201).json({
+      sucess: true,
+      response: result,
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Internal server error",
+    });
+  }
 }
 
 exports.getAllUser = async (req, res) => {
 
-    try {
+  try {
 
-        const result = await User.findAll({
-            attributes:["id","name","email"]
-        });
-    
-        return res.status(201).json({
-          sucess:true,  
-          response: result,
-        });
+    const result = await User.findAll({
+      attributes: ["id", "name", "email"]
+    });
 
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-          msg: "Internal server error",
-        });
-      }
+    return res.status(201).json({
+      sucess: true,
+      response: result,
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Internal server error",
+    });
+  }
 }
 
-exports.signin = async (req,res) => {
+exports.signin = async (req, res) => {
 
-    try {
+  try {
 
-        // write logic to sign in user
-
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-          msg: "Internal server error",
-        });
+    // write logic to sign in user
+    const user = {
+      email: req.body.email,
+      password: req.body.password
+    }
+    const checkUser = await User.findOne({
+      where:
+      {
+        email: user.email
       }
+    })
+    if(!checkUser){
+      return res.status(200).json({
+        "msg":"Invalid Email"
+      })
+    }
+    if (checkUser) {
+      const checkPassword = await bcrypt.compare(user.password, checkUser.password);
+      if (checkPassword) {
+        return res.status(200).json({
+          "msg": 'Login Sucessfull '
+        })
+      }
+      else {
+        return res.status(200).json({
+          "msg": 'Password Invalid'
+        })
+      }
+    }
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Internal server error",
+    });
+  }
 
 }
