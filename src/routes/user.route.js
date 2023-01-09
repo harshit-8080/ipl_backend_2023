0419019c826501a5aa30ef095ef8c657734e8d3e
AayushParashar28 <ayushparashar6203@gmail.com>
2023-01-09 21:01:30 +0530

    Token Generated

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const {auth} = require("../middlewares/token");
 const UserController = require("../controllers/user.controller");
 
 const UserRouter = express.Router();
 
 UserRouter.post("/user", UserController.createUser);
 
-UserRouter.get("/users", UserController.getAllUser);
+UserRouter.get("/users", auth ,UserController.getAllUser);
 
 UserRouter.post("/signin", UserController.signin);
 
