1500c96402348ff7d1e8b87c87bb56e10a23744c
AayushParashar28 <ayushparashar6203@gmail.com>
2023-01-28 15:42:45 +0530

    Check verify code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,14 +13,15 @@ exports.createUser = async (req, res) => {
       password: req.body.password,
       verified: false
     };
+    const otp = otpHelper.generateOTP();
+    user.otp = otp
 
-    this.requestOtp(user.email);
     const result = await User.create(user);
-
-    return res.status(201).json({
-      sucess: true,
-      response: result,
-    });
+    await otpHelper.sendOTP(otp)
+      return res.status(201).json({
+        sucess: true,
+        response: result,
+      });
 
   } catch (error) {
     console.log(error);
@@ -98,61 +99,59 @@ exports.signin = async (req, res) => {
 }
 
 
-exports.requestOtp = async (email) => {
-  try {
-    const checkUser = await User.findOne({
-      where:
-      {
-        email:email
-      }
-    })
-    if (!checkUser) {
-      return res.status(200).json({
-        "msg": "Invalid Email"
-      })
-    }
-    if(checkUser) {
-      const otp = otpHelper.generateOTP();
-      checkUser.otp = otp;
-      await otpHelper.sendOTP(otp)
-      return res.status(200).json({
-        "msg":"OTP Sent Sucessfully"
-      })
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      msg: "Internal server error",
-    });
-  }
-}
+// exports.requestOtp = async (email) => {
+//   try {
+//     const checkUser = await User.findOne({
+//       where:
+//       {
+//         email:email
+//       }
+//     })
+//     if (!checkUser) {
+//       return res.status(200).json({
+//         "msg": "Invalid Email"
+//       })
+//     }
+//     if(checkUser) {
+//       const otp = otpHelper.generateOTP();
+//       checkUser.otp = otp;
+//       await otpHelper.sendOTP(otp)
+//       return res.status(200).json({
+//         "msg":"OTP Sent Sucessfully"
+//       })
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({
+//       msg: "Internal server error",
+//     });
+//   }
+// }
 
-
-exports.verifyOtp = async (req , res) => {
+exports.verifyOtp = async (req, res) => {
   try {
     const checkUser = await User.findOne({
       where:
       {
         email: req.body.email
       }
-    }) 
-    const  otp = req.body.otp 
-    if (!checkUser) {
-      return res.status(200).json({
-        "msg": "Invalid Email"
+    })
+    const otp = req.body.otp ;
+    if(!checkUser){
+      res.status(200).json({
+        "msg":"Invalid User"
       })
     }
-    if(otp !== otp){
-      //console.log(checkUser.otp);
-      console.log(otp)
-       res.status(200).json({
+    if(checkUser && checkUser.otp !== otp ){
+      res.status(200).json({
         "msg":"Invalid OTP"
-       })
-    }else{
-       res.status(200).json({
-        "msg":"OTP Verified Successfully"
-       })
-    } 
+      })
+    }
+    else{
+      res.status(200).json({
+        "msg":"OTP Verified"
+      })
+    }
   } catch (error) {
     console.log(error);
     return res.status(500).json({
